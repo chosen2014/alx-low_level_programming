b18358379bf4c074920eea20c8473ee23bfba73c
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-08 11:33:58 -0700

    Betty style corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>                                                                               
 /**                                                                       * main-print the string in the put function                            
  *
@@ -5,5 +6,7 @@
  * this program prints "{Programming is like building a nultilingual puzzle"
  * Return: 0 after printing the function                              
  */                                                                    
-int main(void)                                                           {
-	puts("\"Programming is like building a multilingual puzzle");            return (0);                                                      }
+int main(void)                                                          
+{
+	puts("\"Programming is like building a multilingual puzzle");            return (0);                                                     
+}
