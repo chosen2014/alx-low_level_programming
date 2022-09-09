1b7a2d721685463a5d70888688a47414bda919e4
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-09 16:19:11 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return 0
+ * Return: 0
  */
-
 int main(void)
 {
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{	
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
 		putchar(ch);
 	}
 	putchar('\n');
