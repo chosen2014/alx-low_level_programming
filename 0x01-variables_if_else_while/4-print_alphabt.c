9029691025ea32e5f16ffbc046bbc34ab91ea045
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-09 17:03:30 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Write a program that prints the alphabet in lowercase, followed by a new line
+ *
+ * Description: using the putchar function
+ * Write a program that prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch != 'e' && ch != 'q')
+	{
+		putchar(ch);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
