2da487af08375eed103fd79a64aaa45ccfb4e002
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-11 01:11:33 -0700

    Betty Compliant

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main -Write a program that prints the alphabet in lowercase, followed by a new line
+ * main -Write a program that prints the alphabet in lowercase, 
+ * followed by a new line
  *
  * Description: using the putchar function
- * Write a program that prints the alphabet in lowercase, followed by a new line
+ * Write a program that prints the alphabet in lowercase, 
+ * followed by a new line
  * Return: 0
  */
 int main(void)
