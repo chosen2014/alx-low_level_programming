b1f8bc47f349a27821c3faaf58a0e51c502f10fc
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-16 04:00:49 -0700

    Done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,7 @@
 void print_triangle(int size)
 {
 	int inc1, inc2;
-	
+
 	if (size > 0)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
@@ -19,17 +19,17 @@ void print_triangle(int size)
 			{
 				putchar(' ');
 			}
-			
+
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
 				putchar('#');
 			}
-			
+
 			if (inc1 == size)
 			{
 				continue;
 			}
-			
+
 			putchar('\n');
 		}
 	}
