d3aa5257f7fd9469e271f2a4c9890728766d7151
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-16 03:48:26 -0700

    Corrected

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,22 +3,23 @@
 
 /**
  * print_triangle - prints a triangle
- * @size: size parameter of triangle
+ * @size:size parameter of triangle
  * Return: returns nothing
  */
 
 void print_triangle(int size)
 {
 	int inc1, inc2;
-
+	
 	if (size > 0)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for ((imc2 = size - inc1); inc2 > 0; inc2--)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				putchar(' ');
 			}
+			
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
 				putchar('#');
