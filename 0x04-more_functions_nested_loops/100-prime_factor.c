8c0f65b537a8e6a1ce1eabc4ca1d29e2e8616e04
Taiwo Araoye <taiwovictor2019@gmail.com>
2022-09-16 04:08:03 -0700

    done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	long number = 612852475143;
 	int inc;
-	
+
 	while (inc++ < number / 2)
 	{
 		if (number % inc == 0)
@@ -19,7 +19,7 @@ int main(void)
 			number /= 2;
 			continue;
 		}
-		
+
 		for (inc = 3; inc < number / 2; inc += 2)
 		{
 			if (number % inc == 0)
